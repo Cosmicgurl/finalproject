0ab4a40760abe3d0b7338ef5b6415869682aa77f
Cosmicgurl <165661217+Cosmicgurl@users.noreply.github.com>
2024-05-15 17:16:54 +0200

    Injecting more weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,33 @@ function refreshData(response){
    let temperatureElement = document.querySelector(".temperature-weather-app");
    let temperature = response.data.temperature.current;
    let cityElement= document.querySelector(".city-weather-app");
-   cityElement.innerHTML= response.data.city;
+   let descriptionElement=document.querySelector("#description");
+   let windspeedElement=document.querySelector("#windspeed");
+   let humidityElement=document.querySelector("#humidity");
+   let timeElement=document.querySelector("#time");
+   let date = new Date(response.data.time * 1000);
+
    temperatureElement.innerHTML = Math.round(temperature);
+   cityElement.innerHTML= response.data.city;
+   descriptionElement.innerHTML = response.data.condition.description;
+   windspeedElement.innerHTML=`${response.data.wind.speed} km/h`;
+   humidityElement.innerHTML=`${response.data.temperature.humidity} %`;
+   timeElement.innerHTML= formatDate(date);
+
+ 
+ console.log(response.data);
+}
+function formatDate(date){
+  let hours = date.getHours();
+  let minutes= date.getMinutes();
+  let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day= days[date.getDay()];
+
+  if(minutes < 10) {
+    minutes = `0${minutes}`
+  }
+
+  return `${day} ${hours}:${minutes}, `
 }
 
 
